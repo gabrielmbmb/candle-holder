4279ddabaf1924bea980ba211c7476e36426af88
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2023-11-14 18:54:31 +0100

    Add all `Bert` modules

--- rusference-models/src/models/bert.rs
+++ rusference-models/src/models/bert.rs
@@ -1,55 +1,68 @@
 use std::collections::HashMap;
 
 use anyhow::Result;
-use candle_core::{IndexOp, Module, Result as CandleResult, Tensor};
-use candle_nn::{linear, Linear, VarBuilder};
-use candle_transformers::models::bert::{BertModel as CandleBertModel, Config as CandleBertConfig};
-use serde::{Deserialize, Serialize};
+use candle_core::{IndexOp, Module, Tensor};
+use candle_nn::{
+    embedding, layer_norm, linear, ops::softmax, Dropout, Embedding, LayerNorm, Linear, VarBuilder,
+};
+use serde::Deserialize;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize)]
 #[serde(rename_all = "lowercase")]
-enum HiddenAct {
+pub enum HiddenAct {
     Gelu,
     Relu,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub struct HiddenActLayer {
+    act: HiddenAct,
+}
+
+impl HiddenActLayer {
+    fn new(act: HiddenAct) -> Self {
+        Self { act }
+    }
+
+    fn forward(&self, xs: &Tensor) -> candle_core::Result<Tensor> {
+        match self.act {
+            HiddenAct::Gelu => xs.gelu(),
+            HiddenAct::Relu => xs.relu(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize, Default)]
 #[serde(rename_all = "lowercase")]
-enum PositionEmbeddingType {
+pub enum PositionEmbeddingType {
     #[default]
     Absolute,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Deserialize)]
 pub struct BertConfig {
-    vocab_size: usize,
-    hidden_size: usize,
-    id2label: Option<HashMap<usize, String>>,
-    num_hidden_layers: usize,
-    num_attention_heads: usize,
-    intermediate_size: usize,
-    hidden_act: HiddenAct,
-    hidden_dropout_prob: f64,
-    attention_probs_dropout_prob: f64,
-    max_position_embeddings: usize,
-    type_vocab_size: usize,
-    initializer_range: f64,
-    layer_norm_eps: f64,
-    pad_token_id: usize,
+    pub vocab_size: usize,
+    pub hidden_size: usize,
+    pub id2label: Option<HashMap<usize, String>>,
+    pub num_hidden_layers: usize,
+    pub num_attention_heads: usize,
+    pub intermediate_size: usize,
+    pub hidden_act: HiddenAct,
+    pub hidden_dropout_prob: f32,
+    pub attention_probs_dropout_prob: f32,
+    pub max_position_embeddings: usize,
+    pub type_vocab_size: usize,
+    pub initializer_range: f64,
+    pub layer_norm_eps: f64,
+    pub pad_token_id: usize,
     #[serde(default)]
-    position_embedding_type: PositionEmbeddingType,
+    pub position_embedding_type: PositionEmbeddingType,
     #[serde(default)]
-    use_cache: bool,
-    classifier_dropout: Option<f64>,
-    model_type: Option<String>,
+    pub use_cache: bool,
+    pub classifier_dropout: Option<f64>,
+    pub model_type: Option<String>,
 }
 
 impl BertConfig {
-    fn to_candle_config(&self) -> CandleBertConfig {
-        let serialized = serde_json::to_string(self).unwrap();
-        serde_json::from_str(&serialized).unwrap()
-    }
-
     fn num_labels(&self) -> usize {
         if let Some(id2label) = &self.id2label {
             return id2label.len();
@@ -83,40 +96,330 @@ impl Default for BertConfig {
     }
 }
 
-#[derive(Debug)]
-struct BertPooler(Linear);
+pub struct BertEmbeddings {
+    word_embeddings: Embedding,
+    position_embeddings: Option<Embedding>,
+    token_type_embeddings: Embedding,
+    layer_norm: LayerNorm,
+    dropout: Dropout,
+}
+
+impl BertEmbeddings {
+    fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let word_embeddings = embedding(
+            config.vocab_size,
+            config.hidden_size,
+            vb.pp("word_embeddings"),
+        )?;
+        let position_embeddings = embedding(
+            config.max_position_embeddings,
+            config.hidden_size,
+            vb.pp("position_embeddings"),
+        )?;
+        let token_type_embeddings = embedding(
+            config.type_vocab_size,
+            config.hidden_size,
+            vb.pp("token_type_embeddings"),
+        )?;
+        let layer_norm = layer_norm(
+            config.hidden_size,
+            config.layer_norm_eps,
+            vb.pp("LayerNorm"),
+        )?;
+        Ok(Self {
+            word_embeddings,
+            position_embeddings: Some(position_embeddings),
+            token_type_embeddings,
+            layer_norm,
+            dropout: Dropout::new(config.hidden_dropout_prob),
+        })
+    }
+
+    fn forward(&self, input_ids: &Tensor, token_type_ids: &Tensor) -> Result<Tensor> {
+        let (_bsize, seq_len) = input_ids.dims2()?;
+        let input_embeddings = self.word_embeddings.forward(input_ids)?;
+        let token_type_embeddings = self.token_type_embeddings.forward(token_type_ids)?;
+        let mut embeddings = (&input_embeddings + token_type_embeddings)?;
+        if let Some(position_embeddings) = &self.position_embeddings {
+            // TODO: Proper absolute positions?
+            let position_ids = (0..seq_len as u32).collect::<Vec<_>>();
+            let position_ids = Tensor::new(&position_ids[..], input_ids.device())?;
+            embeddings = embeddings.broadcast_add(&position_embeddings.forward(&position_ids)?)?
+        }
+        let embeddings = self.layer_norm.forward(&embeddings)?;
+        let embeddings = self.dropout.forward(&embeddings, false)?;
+        Ok(embeddings)
+    }
+}
+
+pub struct BertSelfAttention {
+    query: Linear,
+    key: Linear,
+    value: Linear,
+    dropout: Dropout,
+    num_attention_heads: usize,
+    attention_head_size: usize,
+}
+
+impl BertSelfAttention {
+    fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let attention_head_size = config.hidden_size / config.num_attention_heads;
+        let all_head_size = config.num_attention_heads * attention_head_size;
+        let dropout = Dropout::new(config.attention_probs_dropout_prob);
+        let hidden_size = config.hidden_size;
+        let query = linear(hidden_size, all_head_size, vb.pp("query"))?;
+        let key = linear(hidden_size, all_head_size, vb.pp("key"))?;
+        let value = linear(hidden_size, all_head_size, vb.pp("value"))?;
+        Ok(Self {
+            query,
+            key,
+            value,
+            dropout,
+            num_attention_heads: config.num_attention_heads,
+            attention_head_size,
+        })
+    }
+
+    fn transpose_for_scores(&self, xs: &Tensor) -> candle_core::Result<Tensor> {
+        let mut new_x_shape = xs.dims().to_vec();
+        new_x_shape.pop();
+        new_x_shape.push(self.num_attention_heads);
+        new_x_shape.push(self.attention_head_size);
+        let xs = xs.reshape(new_x_shape.as_slice())?.transpose(1, 2)?;
+        xs.contiguous()
+    }
+}
+
+impl Module for BertSelfAttention {
+    fn forward(&self, hidden_states: &Tensor) -> candle_core::Result<Tensor> {
+        let q = self.query.forward(hidden_states)?;
+        let k = self.key.forward(hidden_states)?;
+        let v = self.value.forward(hidden_states)?;
+
+        let q = self.transpose_for_scores(&q)?;
+        let k = self.transpose_for_scores(&k)?;
+        let v = self.transpose_for_scores(&v)?;
+
+        let attention_scores = q.matmul(&k.t()?)?;
+        let attention_scores = (attention_scores / (self.attention_head_size as f64).sqrt())?;
+        let attention_probs = softmax(&attention_scores, candle_core::D::Minus1)?;
+        let attention_probs = self.dropout.forward(&attention_probs, false)?;
+
+        let context_layer = attention_probs.matmul(&v)?;
+        let context_layer = context_layer.transpose(1, 2)?.contiguous()?;
+        let context_layer = context_layer.flatten_from(candle_core::D::Minus2)?;
+        Ok(context_layer)
+    }
+}
+
+pub struct BertSelfOutput {
+    dense: Linear,
+    layer_norm: LayerNorm,
+    dropout: Dropout,
+}
+
+impl BertSelfOutput {
+    fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let dense = linear(config.hidden_size, config.hidden_size, vb.pp("dense"))?;
+        let layer_norm = layer_norm(
+            config.hidden_size,
+            config.layer_norm_eps,
+            vb.pp("LayerNorm"),
+        )?;
+        let dropout = Dropout::new(config.hidden_dropout_prob);
+        Ok(Self {
+            dense,
+            layer_norm,
+            dropout,
+        })
+    }
+
+    fn forward(
+        &self,
+        hidden_states: &Tensor,
+        input_tensor: &Tensor,
+    ) -> candle_core::Result<Tensor> {
+        let hidden_states = self.dense.forward(hidden_states)?;
+        let hidden_states = self.dropout.forward(&hidden_states, false)?;
+        self.layer_norm.forward(&(hidden_states + input_tensor)?)
+    }
+}
+
+pub struct BertAttention {
+    self_attention: BertSelfAttention,
+    self_output: BertSelfOutput,
+}
+
+impl BertAttention {
+    fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let self_attention = BertSelfAttention::load(vb.pp("self"), config)?;
+        let self_output = BertSelfOutput::load(vb.pp("output"), config)?;
+        Ok(Self {
+            self_attention,
+            self_output,
+        })
+    }
+}
+
+impl Module for BertAttention {
+    fn forward(&self, input_tensor: &Tensor) -> candle_core::Result<Tensor> {
+        let self_outputs = self.self_attention.forward(input_tensor)?;
+        let attention_output = self.self_output.forward(&self_outputs, input_tensor)?;
+        Ok(attention_output)
+    }
+}
+
+pub struct BertIntermediate {
+    dense: Linear,
+    intermediate_act: HiddenActLayer,
+}
+
+impl BertIntermediate {
+    fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let dense = linear(config.hidden_size, config.intermediate_size, vb.pp("dense"))?;
+        Ok(Self {
+            dense,
+            intermediate_act: HiddenActLayer::new(config.hidden_act),
+        })
+    }
+}
+
+impl Module for BertIntermediate {
+    fn forward(&self, xs: &Tensor) -> candle_core::Result<Tensor> {
+        let hidden_states = self.dense.forward(xs)?;
+        let ys = self.intermediate_act.forward(&hidden_states)?;
+        Ok(ys)
+    }
+}
+
+pub struct BertOutput {
+    dense: Linear,
+    layer_norm: LayerNorm,
+    dropout: Dropout,
+}
+
+impl BertOutput {
+    fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let dense = linear(config.intermediate_size, config.hidden_size, vb.pp("dense"))?;
+        let layer_norm = layer_norm(
+            config.hidden_size,
+            config.layer_norm_eps,
+            vb.pp("LayerNorm"),
+        )?;
+        let dropout = Dropout::new(config.hidden_dropout_prob);
+        Ok(Self {
+            dense,
+            layer_norm,
+            dropout,
+        })
+    }
+
+    fn forward(
+        &self,
+        hidden_states: &Tensor,
+        input_tensor: &Tensor,
+    ) -> candle_core::Result<Tensor> {
+        let hidden_states = self.dense.forward(hidden_states)?;
+        let hidden_states = self.dropout.forward(&hidden_states, false)?;
+        self.layer_norm.forward(&(hidden_states + input_tensor)?)
+    }
+}
+
+pub struct BertLayer {
+    attention: BertAttention,
+    intermediate: BertIntermediate,
+    output: BertOutput,
+}
+
+impl BertLayer {
+    fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let attention = BertAttention::load(vb.pp("attention"), config)?;
+        let intermediate = BertIntermediate::load(vb.pp("intermediate"), config)?;
+        let output = BertOutput::load(vb.pp("output"), config)?;
+        Ok(Self {
+            attention,
+            intermediate,
+            output,
+        })
+    }
+}
+
+impl Module for BertLayer {
+    fn forward(&self, hidden_states: &Tensor) -> candle_core::Result<Tensor> {
+        let attention_output = self.attention.forward(hidden_states)?;
+        let intermediate_output = self.intermediate.forward(&attention_output)?;
+        let layer_output = self
+            .output
+            .forward(&intermediate_output, &attention_output)?;
+        Ok(layer_output)
+    }
+}
+
+pub struct BertEncoder {
+    layers: Vec<BertLayer>,
+}
+
+impl BertEncoder {
+    fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let layers = (0..config.num_hidden_layers)
+            .map(|index| BertLayer::load(vb.pp(&format!("layer.{index}")), config))
+            .collect::<Result<Vec<_>>>()?;
+        Ok(Self { layers })
+    }
+}
+
+impl Module for BertEncoder {
+    fn forward(&self, hidden_states: &Tensor) -> candle_core::Result<Tensor> {
+        let mut hidden_states = hidden_states.clone();
+        for layer in self.layers.iter() {
+            hidden_states = layer.forward(&hidden_states)?;
+        }
+        Ok(hidden_states)
+    }
+}
+
+pub struct BertPooler {
+    dense: Linear,
+}
 
 impl BertPooler {
     fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
         let dense = linear(config.hidden_size, config.hidden_size, vb.pp("dense"))?;
-        Ok(Self(dense))
+        Ok(Self { dense })
     }
 }
 
 impl Module for BertPooler {
-    fn forward(&self, hidden_states: &Tensor) -> CandleResult<Tensor> {
+    fn forward(&self, hidden_states: &Tensor) -> candle_core::Result<Tensor> {
         let first_token_tensor = hidden_states.i((.., 0))?;
-        let pooled_output = self.0.forward(&first_token_tensor)?;
+        let pooled_output = self.dense.forward(&first_token_tensor)?;
         pooled_output.tanh()
     }
 }
 
 pub struct BertModel {
-    bert: CandleBertModel,
+    embeddings: BertEmbeddings,
+    encoder: BertEncoder,
     pooler: BertPooler,
 }
 
 impl BertModel {
-    pub fn forward(&self, input_ids: &Tensor, token_type_ids: &Tensor) -> Result<Tensor> {
-        let hidden_states = self.bert.forward(input_ids, token_type_ids)?;
-        let outputs = self.pooler.forward(&hidden_states)?;
-        Ok(outputs)
+    pub fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
+        let embeddings = BertEmbeddings::load(vb.pp("embeddings"), config)?;
+        let encoder = BertEncoder::load(vb.pp("encoder"), config)?;
+        let pooler = BertPooler::load(vb.pp("pooler"), config)?;
+        Ok(Self {
+            embeddings,
+            encoder,
+            pooler,
+        })
     }
 
-    pub fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
-        let pooler = BertPooler::load(vb.pp("pooler"), config)?;
-        let bert = CandleBertModel::load(vb, &config.to_candle_config())?;
-        Ok(Self { bert, pooler })
+    pub fn forward(&self, input_ids: &Tensor, token_type_ids: &Tensor) -> Result<Tensor> {
+        let embedding_output = self.embeddings.forward(input_ids, token_type_ids)?;
+        let sequence_output = self.encoder.forward(&embedding_output)?;
+        let pooled_output = self.pooler.forward(&sequence_output)?;
+        Ok(pooled_output)
     }
 }
 
@@ -128,12 +431,9 @@ pub struct BertForSequenceClassification {
 impl BertForSequenceClassification {
     pub fn load(vb: VarBuilder, config: &BertConfig) -> Result<Self> {
         let classifier = linear(config.hidden_size, config.num_labels(), vb.pp("classifier"))?;
-        let bert_model_with_pooler = BertModel::load(vb.pp("bert"), config)?;
+        let bert = BertModel::load(vb.pp("bert"), config)?;
 
-        Ok(Self {
-            bert: bert_model_with_pooler,
-            classifier,
-        })
+        Ok(Self { bert, classifier })
     }
 
     pub fn forward(&self, input_ids: &Tensor, token_type_ids: &Tensor) -> Result<Tensor> {
