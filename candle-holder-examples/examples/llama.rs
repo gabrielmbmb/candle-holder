3d734ffa8b1889bd9189d0d11bc00cad98df118a
gabrielmbmb <gmartinbdev@gmail.com>
2024-08-21 23:37:29 +0200

    Update example

--- candle-holder-examples/examples/llama.rs
+++ candle-holder-examples/examples/llama.rs
@@ -3,7 +3,7 @@ use candle_holder_examples::get_device_from_args;
 use candle_holder_models::{
     config::GenerationConfig,
     generation::token_streamer::{TextStreamer, TokenStreamer},
-    AutoModelForCausalLM, PreTrainedModel,
+    AutoModelForCausalLM,
 };
 use candle_holder_tokenizers::LlamaTokenizer;
 
@@ -16,11 +16,7 @@ fn main() -> Result<()> {
         AutoModelForCausalLM::from_pretrained("meta-llama/Meta-Llama-3-8B", &device, None, None)?;
 
     let mut encodings = tokenizer
-        .encode(
-            vec!["What do you think about cats?".to_string()],
-            true,
-            None,
-        )
+        .encode(vec!["Once upon a time, in a small village nestled deep within the misty mountains, there lived an old clockmaker named Tobias.".to_string()], true, None)
         .map_err(Error::msg)?;
     encodings.to_device(&device)?;
 
@@ -35,8 +31,8 @@ fn main() -> Result<()> {
             do_sample: true,
             top_p: Some(0.9),
             top_k: Some(50),
-            temperature: 0.5,
-            max_new_tokens: Some(500),
+            temperature: 0.7,
+            max_new_tokens: Some(5000),
             ..GenerationConfig::default()
         }),
         Some(token_streamer),
@@ -44,7 +40,7 @@ fn main() -> Result<()> {
     )?;
 
     let sequence = &generation[0][..];
-    let text = tokenizer.decode(sequence, true)?;
+    tokenizer.decode(sequence, true)?;
 
     println!("Took: {:?}", start.elapsed());
 
