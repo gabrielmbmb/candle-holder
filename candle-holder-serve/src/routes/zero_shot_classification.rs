dd989b693681c7e6ffe258ef7a0ebd47edcdb63a
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-09-04 15:23:39 +0200

    Add zero shot classification router

--- candle-holder-serve/src/routes/zero_shot_classification.rs
+++ candle-holder-serve/src/routes/zero_shot_classification.rs
@@ -1,32 +1,117 @@
-use anyhow::Result;
-use axum::{routing::post, Router};
-use candle_holder_pipelines::ZeroShotClassificationPipeline;
-use std::sync::Arc;
+use candle_holder_pipelines::{ZeroShotClassificationOptions, ZeroShotClassificationPipeline};
+use serde::{Deserialize, Serialize};
 
-use crate::cli::Cli;
+use crate::generate_router;
 
-pub fn router(args: &Cli) -> Result<Router> {
-    let model = args.model();
-    let device = args.device()?;
-
-    tracing::info!(
-        "Loading zero shot classification pipeline for model '{}' on device {:?}",
-        model,
-        device
-    );
-
-    let pipeline = Arc::new(ZeroShotClassificationPipeline::new(
-        &args.model(),
-        &args.device()?,
-        None,
-        None,
-    )?);
-
-    Ok(Router::new()
-        .route("/", post(inference))
-        .with_state(pipeline))
+#[derive(Debug, Clone, Deserialize)]
+struct ZeroShotClassificationInferenceParams {
+    candidate_labels: Vec<String>,
+    #[serde(default)]
+    hypothesis_template: Option<String>,
+    #[serde(default)]
+    multi_label: Option<bool>,
 }
 
-async fn inference() -> &'static str {
-    "inference"
+impl Default for ZeroShotClassificationInferenceParams {
+    fn default() -> Self {
+        Self {
+            candidate_labels: vec![],
+            hypothesis_template: None,
+            multi_label: None,
+        }
+    }
+}
+
+impl From<ZeroShotClassificationInferenceParams> for ZeroShotClassificationOptions {
+    fn from(params: ZeroShotClassificationInferenceParams) -> Self {
+        let default = ZeroShotClassificationOptions::default();
+        Self {
+            hypothesis_template: params
+                .hypothesis_template
+                .unwrap_or(default.hypothesis_template),
+            multi_label: params.multi_label.unwrap_or(default.multi_label),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+enum Inputs {
+    Single(String),
+    Multiple(Vec<String>),
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub(crate) struct ZeroShotClassificationInferenceRequest {
+    inputs: Inputs,
+    parameters: Option<ZeroShotClassificationInferenceParams>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub(crate) struct ZeroShotClassificationResult {
+    sequence: String,
+    labels: Vec<String>,
+    scores: Vec<f32>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+#[serde(untagged)]
+pub(crate) enum ZeroShotClassificationInferenceResponse {
+    Single(ZeroShotClassificationResult),
+    Multiple(Vec<ZeroShotClassificationResult>),
+}
+
+generate_router!(
+    ZeroShotClassificationPipeline,
+    ZeroShotClassificationInferenceRequest,
+    ZeroShotClassificationInferenceResponse,
+    process_text_classification
+);
+
+pub(crate) fn process_text_classification(
+    pipeline: &ZeroShotClassificationPipeline,
+    request: ZeroShotClassificationInferenceRequest,
+) -> ZeroShotClassificationInferenceResponse {
+    let params = request.parameters.unwrap_or_default();
+
+    match request.inputs {
+        Inputs::Single(text) => {
+            let output = pipeline
+                .run(
+                    text.clone(),
+                    params.candidate_labels.clone(),
+                    Some(params.into()),
+                )
+                .unwrap();
+            let (labels, scores): (Vec<String>, Vec<f32>) = output.into_iter().unzip();
+            let result = ZeroShotClassificationResult {
+                sequence: text,
+                labels,
+                scores,
+            };
+            ZeroShotClassificationInferenceResponse::Single(result)
+        }
+        Inputs::Multiple(texts) => {
+            let outputs = pipeline
+                .run_batch(
+                    texts.clone(),
+                    params.candidate_labels.clone(),
+                    Some(params.into()),
+                )
+                .unwrap();
+            let results = outputs
+                .into_iter()
+                .zip(texts.into_iter())
+                .map(|(output, text)| {
+                    let (labels, scores): (Vec<String>, Vec<f32>) = output.into_iter().unzip();
+                    ZeroShotClassificationResult {
+                        sequence: text,
+                        labels,
+                        scores,
+                    }
+                })
+                .collect();
+            ZeroShotClassificationInferenceResponse::Multiple(results)
+        }
+    }
 }
