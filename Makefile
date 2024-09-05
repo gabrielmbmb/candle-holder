5b7207b96b19204f5a863ca30205c302bc3cf186
gabrielmbmb <gmartinbdev@gmail.com>
2024-09-05 16:48:00 +0200

    Update `Dockerfile.cuda`

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: build-docker
 build-docker:
 	docker build -f candle-holder-serve/Dockerfile -t gabrielmbmb/candle-holder-serve:latest .
+
+.PHONY: build-docker-cuda
+build-docker-cuda:
+	docker build -f candle-holder-serve/Dockerfile.cuda -t gabrielmbmb/candle-holder-serve:cuda-latest .
---
+++ candle-holder-serve/entrypoint-cuda.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Based on https://github.com/huggingface/text-embeddings-inference/blob/main/cuda-all-entrypoint.sh
+if ! command -v nvidia-smi &>/dev/null; then
+  echo "Error: 'nvidia-smi' command not found."
+  exit 1
+fi
+
+compute_cap=$(nvidia-smi --query-gpu=compute_cap --format=csv | sed -n '2p' | sed 's/\.//g')
+
+ARGS=()
+
+if [ -z "$CANDLE_HOLDER_MODEL" ] || [ -z "$CANDLE_HOLDER_PIPELINE" ]; then
+  echo "Error: Both CANDLE_HOLDER_MODEL and CANDLE_HOLDER_PIPELINE must be provided."
+  exit 1
+fi
+
+if [ ! -z "$CANDLE_HOLDER_MODEL" ]; then
+  ARGS+=("--model" "$CANDLE_HOLDER_MODEL")
+fi
+
+if [ ! -z "$CANDLE_HOLDER_PIPELINE" ]; then
+  ARGS+=("--pipeline" "$CANDLE_HOLDER_PIPELINE")
+fi
+
+if [ ! -z "$CANDLE_HOLDER_DEVICE" ]; then
+  ARGS+=("--device" "$CANDLE_HOLDER_DEVICE")
+fi
+
+if [ ! -z "$CANDLE_HOLDER_DTYPE" ]; then
+  ARGS+=("--dtype" "$CANDLE_HOLDER_DTYPE")
+fi
+
+if [ ! -z "$CANDLE_HOLDER_NUM_WORKERS" ]; then
+  ARGS+=("--num-workers" "$CANDLE_HOLDER_NUM_WORKERS")
+fi
+
+CANDLE_HOLDER_HOST=${CANDLE_HOLDER_HOST:-0.0.0.0:8080}
+ARGS+=("--host" "$CANDLE_HOLDER_HOST")
+
+if [ ${compute_cap} -eq 75 ]; then
+  exec candle-holder-serve-cuda75 "${ARGS[@]}"
+elif [ ${compute_cap} -ge 80 -a ${compute_cap} -lt 90 ]; then
+  exec candle-holder-serve-cuda80 "${ARGS[@]}"
+elif [ ${compute_cap} -eq 90 ]; then
+  exec candle-holder-serve-cuda90 "${ARGS[@]}"
+else
+  echo "cuda compute cap ${compute_cap} is not supported"
+  exit 1
+fi
