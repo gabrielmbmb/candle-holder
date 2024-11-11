38df7b5ba2bf31c2dc3270cfa7077aaa27cef22f
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2024-11-11 09:27:29 -0400

    Update example

--- candle-holder-examples/examples/streamed_text_generation/main.rs
+++ candle-holder-examples/examples/streamed_text_generation/main.rs
@@ -4,14 +4,14 @@ extern crate accelerate_src;
 #[cfg(feature = "mkl")]
 extern crate intel_mkl_src;
 
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use anyhow::{Error, Result};
 use candle_holder_examples::Cli;
 use candle_holder_models::{
-    AutoModelForCausalLM, GenerationConfig, GenerationParams, TextStreamer, TokenStreamer, TextIteratorStreamer
+    AutoModelForCausalLM, GenerationConfig, GenerationParams, TextStreamer
 };
-use candle_holder_tokenizers::{AutoTokenizer, Message};
+use candle_holder_tokenizers::{AutoTokenizer, BatchEncoding, Message, Tokenizer};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -44,6 +44,30 @@ pub struct GenerationCli {
     pub apply_chat_template: bool,
 }
 
+fn encode(
+    tokenizer: Arc<dyn Tokenizer>,
+    prompt: String,
+    system_prompt: Option<String>,
+    apply_chat_template: bool,
+    device: &candle_core::Device,
+) -> Result<BatchEncoding> {
+    let mut encodings = if apply_chat_template {
+        let mut messages = vec![Message::user(prompt)];
+        if let Some(system_prompt) = system_prompt {
+            messages.insert(0, Message::system(system_prompt))
+        }
+        tokenizer
+            .apply_chat_template_and_encode(messages, true)
+            .map_err(Error::msg)?
+    } else {
+        tokenizer
+            .encode(vec![prompt], true, None)
+            .map_err(Error::msg)?
+    };
+    encodings.to_device(device)?;
+    Ok(encodings)
+}
+
 fn main() -> Result<()> {
     let args = GenerationCli::parse();
 
@@ -53,36 +77,24 @@ fn main() -> Result<()> {
     let tokenizer = AutoTokenizer::from_pretrained(args.model.clone(), None, None)?;
     let model = AutoModelForCausalLM::from_pretrained(args.model, &device, None, None)?;
 
-    let mut encodings = if args.apply_chat_template {
-        let mut messages = vec![Message::user(args.prompt.clone())];
-        if let Some(system_prompt) = args.system_prompt {
-            messages.insert(0, Message::system(system_prompt))
-        }
-        tokenizer
-            .apply_chat_template_and_encode(messages, true)
-            .map_err(Error::msg)?
-    } else {
-        tokenizer
-            .encode(vec![args.prompt], true, None)
-            .map_err(Error::msg)?
-    };
-    encodings.to_device(&device)?;
-
     let start = std::time::Instant::now();
 
-    let token_streamer: Box<dyn TokenStreamer> = Box::new(TextStreamer::new(
-        &tokenizer,
+    let token_streamer = Box::new(TextStreamer::new(
+        tokenizer.clone(),
         args.apply_chat_template,
         true,
     ));
 
-    // let token_streamer: Box<dyn TokenStreamer> = Box::new(TextIteratorStreamer::new(
-    //     &tokenizer,
-    //     args.apply_chat_template,
-    //     true,
-    // ));
+    let encodings = encode(
+        tokenizer.clone(),
+        args.prompt,
+        args.system_prompt,
+        args.apply_chat_template,
+        &device,
+    )?;
 
     let input_ids = encodings.get_input_ids();
+
     model.generate(
         input_ids,
         GenerationParams {
@@ -94,7 +106,7 @@ fn main() -> Result<()> {
                 max_new_tokens: Some(args.max_new_tokens),
                 ..GenerationConfig::default()
             }),
-            tokenizer: Some(&tokenizer),
+            tokenizer: Some(tokenizer.clone()),
             token_streamer: Some(token_streamer),
             ..Default::default()
         },
